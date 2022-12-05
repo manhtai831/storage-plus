40f5d880568bf27524f4ac73f512da83dfa32dcb
Do Manh Tai <manhtai831@gmail.com>
2022-12-05 17:29:35 +0700

    update

--- lib/src/sqlite/extension/extension.dart
+++ lib/src/sqlite/extension/extension.dart
@@ -1,3 +1,7 @@
+import 'dart:convert';
+
+import 'package:flutter/services.dart';
+
 extension BooleanExtension on num? {
   bool get toBool => this == 1;
 }
@@ -7,4 +11,12 @@ extension IntegerExtension on bool? {
     if (this == true) return 1;
     return 0;
   }
+}
+
+extension Uint8ListExtension on Map<String, dynamic>? {
+  Uint8List get toBytes => Uint8List.fromList(jsonEncode(this).codeUnits);
+}
+
+extension StringExtension on Uint8List? {
+  String get string => String.fromCharCodes(this ?? []);
 }
