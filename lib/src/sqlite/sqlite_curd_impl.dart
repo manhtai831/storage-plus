8556425d4b05cd2356b7ed01c04bf3d3e54f37cd
Do Manh Tai <manhtai831@gmail.com>
2022-12-08 14:12:20 +0700

    update

--- lib/src/sqlite/sqlite_curd_impl.dart
+++ lib/src/sqlite/sqlite_curd_impl.dart
@@ -25,7 +25,7 @@ class SqliteCurdImpl implements ISqliteIU, ISqliteRD, ISqliteRaw {
   @override
   Future<void> delete() async {
     _rawQuery.write('DELETE FROM $_tableName ${_rawWhere.toString()}');
-    await _mDatabase.execute(_rawQuery.toString());
+    await _mDatabase.rawDelete(_rawQuery.toString(),_values);
   }
 
   @override
